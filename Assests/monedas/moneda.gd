dd8c46b1e5d47d59d25cb1834dbf5a7e724f135f
JorvisGS <jorvidjose@gmail.com>
2025-05-01 19:20:56 +0200

    summmit

--- Assests/monedas/moneda.gd
+++ Assests/monedas/moneda.gd
@@ -2,6 +2,7 @@ extends Area2D
 # Called when the node enters the scene tree for the first time.
 
 signal collected
+@onready var collision_shape_2d: CollisionShape2D = $CollisionShape2D
 @onready var maquinaEstados: AnimationTree = $AnimationTree
 func moneda_normal(actiado:bool):
 	maquinaEstados["parameters/conditions/normal"] = actiado
@@ -14,11 +15,15 @@ func _ready():
 	connect("body_entered", _on_body_entered)
 
 func _on_body_entered(body):
+	collision_shape_2d.disabled = true
 	if body.is_in_group("jugador"):  # or check for group if you use groups
 		emit_signal("collected")
 		moneda_agarrada(true)
+		self.monitoring = false
+		set_deferred("monitoring", false)
 		var tween = create_tween()
 		tween.tween_property(self, "position", position + Vector2(0, -15), 0.5)
 		tween.tween_property(self, "modulate:a", 0.0, 0.5)
 		tween.tween_callback(queue_free)
 		#queue_free()  # remove the coin
+	
