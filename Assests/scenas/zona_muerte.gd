0f54f941c0a767bdf3e8d6d320fddbf1ed8879eb
fio-nose <fiorelh88@gmail.com>
2025-05-02 13:50:56 +0200

    PORFIN
    Corazones, d momento 3, q desaparecen uno a uno al hacerse daño

--- Assests/scenas/zona_muerte.gd
+++ Assests/scenas/zona_muerte.gd
@@ -4,6 +4,8 @@ extends Area2D
 func _on_body_entered(body: Node2D) -> void:
 	if body.is_in_group("jugador"):
 		if body.has_method("reproducir_animacion_daño"):
+			var canvas_layer = get_parent().get_node("../CanvasLayer")#
+			canvas_layer.take_damage()#
 			body.reproducir_animacion_daño()
 			await get_tree().create_timer(1.5).timeout  # espera 1.5 segundos
 			
--- gui_gato.gd
+++ gui_gato.gd
@@ -1,13 +1,36 @@
 extends CanvasLayer
 @onready var moneda_label=$moneda_label
 var coin_count = 0
+@export var heart_texture : Texture2D
+var max_hearts := 3
+var current_hearts := 3
+
+@onready var hearts := [
+	$HBoxContainer/TextureRect,
+	$HBoxContainer/TextureRect2,
+	$HBoxContainer/TextureRect3
+]
+
+func take_damage(amount: int = 1):
+	current_hearts = max(current_hearts - amount, 0)
+	update_hearts()
+	if current_hearts <= 0:
+		print("¡Jugador muerto!")
+
+func update_hearts():
+	for i in range(max_hearts):
+		if i < current_hearts:
+			hearts[i].show()
+		else:
+			hearts[i].hide()
+
 func _on_moneda_collected():
 	coin_count += 1
 	moneda_label.text="X: "+ str(coin_count)
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	update_hearts()
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
