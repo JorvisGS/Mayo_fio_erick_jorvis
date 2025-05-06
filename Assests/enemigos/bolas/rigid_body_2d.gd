d80e8579b32028e47ec122d89bce6e9c9659ddee
fio-nose <fiorelh88@gmail.com>
2025-05-06 18:01:00 +0200

    bolas_update

---
+++ Assests/enemigos/bolas/rigid_body_2d.gd
@@ -0,0 +1,17 @@
+extends RigidBody2D
+
+@onready var raycast = $RayCast2D
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	apply_impulse(Vector2(20, -30))
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _physics_process(delta: float) -> void:
+	if raycast.is_colliding():
+		var collider = raycast.get_collider()
+		# Si el raycast está tocando el suelo (o el terreno), la pelota puede continuar
+		if collider.is_in_group("suelo"):
+			pass
+		else:
+			pass
